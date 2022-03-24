9cbd5c1eb5f86c1a6f3294505bd4dc3d82ab6d61
MinervaD <minervadiago@gmail.com>
2022-03-24 01:50:32 -0600

    se agregó la función de decremento

--- contador/app.js
+++ contador/app.js
@@ -1,11 +1,18 @@
-// inicializando nuestro contador
+// inicializando nuestros contadores
 let count = 0
-
+let count2 = 0
 let value = document.getElementById('contador')
-const btnIncrement = document.getElementsByClassName('.increment')
 
+const btnIncrement = document.getElementsByClassName('.increment')
+const btnDecrement = document.getElementsByClassName('.decrement')
 
 function increment() {
     value = count++
     document.getElementById('contador').innerHTML = value
+    count2=value
+}
+function decrement() {
+    value = count2--
+    document.getElementById('contador').innerHTML = value
+    count=value
 }
